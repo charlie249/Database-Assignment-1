9274ad52f5a0fbcff67612d957575d990d1af75a
charlie249 <56076410+charlie249@users.noreply.github.com>
2020-05-22 15:31:48 +0100

    Part 3 Python Code task started

---
+++ Part 3/Part 3.py
@@ -0,0 +1,20 @@
+import sqlite3
+db = sqlite3.connect('/Users/charlie/Documents/Git/Database-Assignment-1/Assignment _1_Database.db')
+cursor = db.cursor()
+shopper_id = input("Enter a shopper_id: ")
+print("ORINOCO - SHOPPER MAIN MENU")
+print("•    Display your order history")
+print("•    Add an item to your basket")
+print("•    View your basket")
+print("•    Checkout")
+print("•    Exit")
+
+sql_query = "SELECT order_id, order_date \
+		   FROM shopper_orders"
+cursor.execute(sql_query)
+all_sell_rows = cursor.fetchall()
+if all_sell_rows:
+
+else:
+    print("No seller found")
+db.close()
